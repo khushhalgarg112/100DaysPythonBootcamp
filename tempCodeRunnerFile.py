3a1998ff0838760cff1926cce3d6d9875ac48f58
Khushhal Garg <khusgarg1@gmail.com>
2023-06-18 15:54:36 +0530

    image for choosing colors

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,15 @@
+from turtle import Turtle, Screen
 
+part = Turtle()
+part.color("red", "green")
+part.speed(5)
+# while True:
+#     part.forward(100)
+#     part.right(170)
+
+part.forward(100)
+part.right(90)
+part.forward(100)
+part.right(90)
+part.forward(100)
+part.right(90)
