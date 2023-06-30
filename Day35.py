264be96212baede4757fb81915312c311cddc5b0
Khushhal Garg <khusgarg1@gmail.com>
2023-06-30 23:46:12 +0530

    Weather Report SMS sender

--- Day35.py
+++ Day35.py
@@ -1,27 +1,24 @@
 import requests
 from twilio.rest import Client
 
-api_key = 'api'
-account_sid = 'AC47f7d48de5d76ea50ff0765acbaf333f'
-auth_token = 'twilio auth token'
+api_key = "api"
+account_sid = "twilio account sid"
+auth_token = "twilio auth token"
 
-parameter = {
-    "lat": 29.353861,
-    "lon": 76.495880,
-    "appid": api_key
-}
+parameter = {"lat": 29.353861, "lon": 76.495880, "appid": api_key}
 
-response = requests.get("https://api.openweathermap.org/data/2.5/weather", params=parameter)
+response = requests.get(
+    "https://api.openweathermap.org/data/2.5/weather", params=parameter
+)
 response.raise_for_status()
 data = response.json()["weather"][0]["id"]
 if data < 700:
-
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
-    from_='your twilio number',
-    body='its raining please keep your umbrella',
-    to='Number where to send message'
+        from_="your twilio number",
+        body="its raining please keep your umbrella",
+        to="Number where to send message",
     )
 
     print(message.status)
