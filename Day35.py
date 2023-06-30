c8bee5e99af8e828283fa3bce4a4c712aa623fb1
Khushhal Garg <khusgarg1@gmail.com>
2023-06-30 23:41:42 +0530

    Weather Report SMS sender

--- Day35.py
+++ Day35.py
@@ -1,9 +1,9 @@
 import requests
 from twilio.rest import Client
 
-api_key = '04fc3a252670479cd2189c226194a31a'
+api_key = 'api'
 account_sid = 'AC47f7d48de5d76ea50ff0765acbaf333f'
-auth_token = 'd439d9c124efed77af9a96e56db2f238'
+auth_token = 'twilio auth token'
 
 parameter = {
     "lat": 29.353861,
@@ -19,9 +19,9 @@ if data < 700:
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
-    from_='+14178554309',
+    from_='your twilio number',
     body='its raining please keep your umbrella',
-    to='+918307191801'
+    to='Number where to send message'
     )
 
     print(message.status)
