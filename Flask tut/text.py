457ae819f3cc8605748cd90809ef19dd0e3dff9b
Khushhal Garg <khusgarg1@gmail.com>
2023-07-14 22:34:24 +0530

    Flask Basics

---
+++ Flask tut/text.py
@@ -0,0 +1,16 @@
+# import time
+
+# def hi(fun):
+#     def ni():
+#         time.sleep(2)
+#         fun()
+    
+#     return ni
+
+# @hi 
+# def say():
+#     print("hello")
+# def y():
+#     print("hi") 
+
+# say()
